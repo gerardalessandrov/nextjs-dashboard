09373e4a8e7b0324fd98b1086f0c0d006a120a59
gerardalessandrov <gerardvigorodrigues@gmail.com>
2025-12-02 23:31:21 -0500

    definition

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,7 +1,4 @@
-// This file contains type definitions for your data.
-// It describes the shape of the data, and what data type each property should accept.
-// For simplicity of teaching, we're manually defining these types.
-// However, these types are generated automatically if you're using an ORM such as Prisma.
+// definitions.ts
 export type User = {
   id: string;
   name: string;
@@ -21,8 +18,6 @@ export type Invoice = {
   customer_id: string;
   amount: number;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
 
@@ -30,6 +25,7 @@ export type Revenue = {
   month: string;
   revenue: number;
 };
+
 export type LatestInvoice = {
   id: string;
   name: string;
@@ -38,7 +34,6 @@ export type LatestInvoice = {
   amount: string;
 };
 
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
 };
@@ -62,16 +57,6 @@ export type CustomersTableType = {
   total_invoices: number;
   total_pending: number;
   total_paid: number;
-};
-
-export type FormattedCustomersTable = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: string;
-  total_paid: string;
 };
 
 export type CustomerField = {
