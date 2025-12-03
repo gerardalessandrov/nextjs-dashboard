2d2d26cac9c37d8b5a7e9a95e9045f135a3d595f
gerardalessandrov <gerardvigorodrigues@gmail.com>
2025-12-03 10:03:10 -0500

    definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -5,6 +5,15 @@ export type User = {
   email: string;
   password: string;
 };
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
 
 export type Customer = {
   id: string;
